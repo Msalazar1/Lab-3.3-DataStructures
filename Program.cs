c4722362bd24118be75ac55c8a0598d72eaf692d
Manny <mannysalazar@quickenloans.com>
2020-07-20 08:53:07 -0400

    Lab 3.3 Data Structures

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			while (true)
+			{
+
+				Console.Write("Want to reverse a word? Type it here: ");
+				string input = Console.ReadLine();
+
+
+				Console.WriteLine($"Your word in reverse is: {Reverse(input)}");
+
+
+				Console.Write("\nWant to reverse another word? yes or no: ");
+				string keepgoing = Console.ReadLine().ToLower();
+				while (!(keepgoing == "yes" || keepgoing == "no"))
+				{
+					Console.Write("\n Oops!  Something went wrong.  Please enter yes or no: ");
+					keepgoing = Console.ReadLine().ToLower();
+				}
+				if (keepgoing == "no" || keepgoing == "n")
+				{
+					break;
+				}
+				else
+				{
+					Console.Clear();
+				}
+			}
+		}
+		static string Reverse(string sentence)
+		{
+			string reversedSentence = "";
+
+			string[] words = sentence.Split(' ');
+
+			foreach (string word in words)
+			{
+				string reversedWord = "";
+
+				Stack<char> reverseOrder = new Stack<char>();
+
+				char[] letters = word.ToCharArray();
+				foreach (char letter in letters)
+				{
+					reverseOrder.Push(letter);
+				}
+
+				while (reverseOrder.TryPop(out char currentLetter))
+				{
+					reversedWord += currentLetter;
+				}
+
+				reversedSentence += (reversedWord + " ");
+			}
+
+			return reversedSentence;
+		}
+	}
+}
